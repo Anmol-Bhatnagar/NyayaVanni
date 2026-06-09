12d30ee4730bf6c441bc23132c6668f21166c7ca
NIDHI SINGH <nidhi14a@gmail.com>
2026-06-09 21:51:57 +0530

    Fix footer layout (#352)

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -269,7 +269,7 @@ export default function ContactUs() {
         </main>
       </div>
 
-      <section className="z-10 w-full px-6 pb-16 mx-auto max-w-7xl">
+      <section className="z-10 w-full">
         <Footer />
       </section>
     </div>
